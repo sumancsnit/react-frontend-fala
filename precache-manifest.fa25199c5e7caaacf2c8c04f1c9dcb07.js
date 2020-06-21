ea512a66ca7f6bdd6fc3b7736160485486a56c3e
Suman Kumar <sumancsnit@gmail.com>
2020-06-22 00:15:25 +0530

    Updates

---
+++ precache-manifest.fa25199c5e7caaacf2c8c04f1c9dcb07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68cdec7e7b6d8b58ef6e38f93d73ff3d",
+    "url": "/react-frontend-fala/index.html"
+  },
+  {
+    "revision": "37675c2cee7e3e03b1fd",
+    "url": "/react-frontend-fala/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c6bde343ddef2934162b",
+    "url": "/react-frontend-fala/static/js/2.8a70e0d2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-frontend-fala/static/js/2.8a70e0d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37675c2cee7e3e03b1fd",
+    "url": "/react-frontend-fala/static/js/main.73d85393.chunk.js"
+  },
+  {
+    "revision": "a71a02fd4de1ecd8f5ec",
+    "url": "/react-frontend-fala/static/js/runtime-main.85e5e84a.js"
+  }
+]);
